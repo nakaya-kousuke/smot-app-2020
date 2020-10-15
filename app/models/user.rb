1e7b98b6ab579f7bfe2fe2b467f39b2207088561
nakaya-kousuke <i.my.me.ode23@gmail.com>
2020-10-15 19:42:46 +0900

    favoritesテーブルの作成 / モデルのアソシエーションやバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
+  has_many :favorites, dependent: :destroy 
+
   validates :family_name, presence: true
   validates :first_name,  presence: true
   validates :gender,      presence: {massage: "選択して下さい"}
